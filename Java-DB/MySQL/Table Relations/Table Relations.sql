f359903e5fca642b0849a7f494901091c8fb85f9
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-06-10 12:15:49 +0300

    Table Relations

--- Java-DB/MySQL/Table Relations/Table Relations.sql
+++ Java-DB/MySQL/Table Relations/Table Relations.sql
@@ -131,8 +131,114 @@ FOREIGN KEY (`manager_id`)
 REFERENCES`teachers`(`teacher_id`);
 
 #05. Online Store Database
+CREATE TABLE `cities`(
+`city_id` INT(11) PRIMARY KEY,
+`name` VARCHAR(50));
+
+CREATE TABLE `customers`(
+`customer_id` INT(11) PRIMARY KEY,
+`name` VARCHAR(50),
+`birthday` DATE,
+`city_id` INT,
+CONSTRAINT fk_customers_cities
+FOREIGN KEY (`city_id`)
+REFERENCES `cities`(`city_id`)
+);
+
+CREATE TABLE `orders`(
+`order_id` INT(11) PRIMARY KEY,
+`customer_id` INT(11),
+CONSTRAINT fk_orders_customers
+FOREIGN KEY (`customer_id`)
+REFERENCES `customers`(`customer_id`)
+ON DELETE CASCADE
+ON UPDATE CASCADE
+);
+
+CREATE TABLE item_types(
+`item_type_id` INT(11) PRIMARY KEY,
+`name` VARCHAR(50)
+);
+
+CREATE TABLE `items`(
+`item_id` INT(11) PRIMARY KEY,
+`name` VARCHAR(50),
+item_type_id INT,
+CONSTRAINT fk_items_item_types
+FOREIGN KEY (`item_type_id`)
+REFERENCES `item_types`(`item_type_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+CREATE TABLE `order_items`(
+`order_id` INT(11),
+`item_id` INT(11),
+CONSTRAINT pk_order_items
+PRIMARY KEY (`order_id`, `item_id`),
+CONSTRAINT fk_order_items_orders
+FOREIGN KEY(`order_id`)
+REFERENCES `orders`(`order_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE,
+CONSTRAINT fk_orders_items_items
+FOREIGN KEY(`item_id`)
+REFERENCES `items`(`item_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
 
 #06. University Database
+CREATE TABLE `subjects`(
+`subject_id` INT PRIMARY KEY,
+`subject_name` VARCHAR(50)
+);
+
+CREATE TABLE `majors`(
+`major_id` INT PRIMARY key,
+`name` VARCHAR(50)
+);
+
+CREATE TABLE `students`(
+`student_id` INT PRIMARY KEY,
+`student_number` VARCHAR(12) UNIQUE,
+`student_name` VARCHAR(50),
+`major_id` INT,
+CONSTRAINT fk_students_majors
+FOREIGN KEY (`major_id`)
+REFERENCES `majors`(`major_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+CREATE TABLE `agenda`(
+`student_id` INT,
+`subject_id` INT,
+CONSTRAINT pk_agenda
+PRIMARY KEY (`student_id`, `subject_id`),
+CONSTRAINT fk_agenda_students
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE,
+CONSTRAINT fk_agenda_subjects
+FOREIGN KEY (`subject_id`)
+REFERENCES `subjects`(`subject_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+CREATE TABLE `payments`(
+`payment_id` INT PRIMARY KEY,
+`payment_date` DATE,
+`payment_amount` DECIMAL(8,2),
+`student_id` INT,
+CONSTRAINT fk_payments_students
+FOREIGN KEY(`student_id`)
+REFERENCES `students`(`student_id`)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
 
 USE `geography`;
 
