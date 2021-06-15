009a0eacf3da8c52a8c7e1e8e7f1da6002924b5f
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-06-15 17:25:16 +0300

    Update Subqueries and JOINs.sql

--- Java-DB/MySQL/Subqueries and JOINs/Subqueries and JOINs.sql
+++ Java-DB/MySQL/Subqueries and JOINs/Subqueries and JOINs.sql
@@ -6,4 +6,14 @@ FROM `employees` AS e
 JOIN `addresses` AS a
 USING(`address_id`)
 ORDER BY a.`address_id`
-LIMIT 5; 
+LIMIT 5;
+
+#02. Addresses with Towns
+SELECT e.`first_name`, e.`last_name`, t.`name` AS `town`, a.`address_text`
+FROM `employees` AS e
+JOIN `addresses` AS a
+ON e.`address_id` = a.`address_id`
+JOIN `towns` AS t
+ON t.`town_id` = a.`town_id`
+ORDER BY e.`first_name`, e.`last_name`
+LIMIT 5;
