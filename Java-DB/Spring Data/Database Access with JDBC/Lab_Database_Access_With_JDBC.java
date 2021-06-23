883de2f3bc49d6c51b9b79179c5fca9f84226eba
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-06-23 16:53:52 +0300

    Update Lab_Database_Access_With_JDBC.java

--- Java-DB/Spring Data/Database Access with JDBC/Lab_Database_Access_With_JDBC.java
+++ Java-DB/Spring Data/Database Access with JDBC/Lab_Database_Access_With_JDBC.java
@@ -6,27 +6,23 @@ import java.util.Scanner;
 
 public class Lab_Database_Access_With_JDBC {
     public static void main(String[] args) throws SQLException {
-
         Scanner scan = new Scanner(System.in);
 
-        Properties props = new Properties();
-        props.setProperty("user", "root");
-        props.setProperty("password", "1234");
-
-        Connection soft_uniConnection =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/soft_uni", props);
-        Connection diabloConnection =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", props);
-
+        System.out.println("Exercise: ");
         int exNum = Integer.parseInt(scan.nextLine());
 
         switch (exNum) {
-            case 1 -> ex_1(scan, soft_uniConnection);
-            case 2 -> ex_2(scan, diabloConnection);
+            case 1 -> ex_1(scan);
+            case 2 -> ex_2(scan);
         }
     }
 
-    private static void ex_1(Scanner scan, Connection connection) throws SQLException {
+    private static void ex_1(Scanner scan) throws SQLException {
+        System.out.println("Enter salary: ");
+
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/soft_uni", getProperties());
+
         PreparedStatement stmt =
                 connection.prepareStatement("SELECT `first_name`, `last_name`FROM `employees`WHERE `salary` > ?");
         double salary = Double.parseDouble(scan.nextLine());
@@ -34,13 +30,18 @@ public class Lab_Database_Access_With_JDBC {
 
         ResultSet rs = stmt.executeQuery();
 
-        while(rs.next()){
+        while (rs.next()) {
             System.out.println(rs.getString("first_name")
                     + " " + rs.getString("last_name"));
         }
     }
 
-    private static void ex_2(Scanner scan, Connection connection) throws SQLException {
+    private static void ex_2(Scanner scan) throws SQLException {
+        System.out.println("Enter username: ");
+
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://localhost:3306/diablo", getProperties());
+
         PreparedStatement stmt =
                 connection.prepareStatement("SELECT u.`user_name`, u.`first_name`, u.`last_name`, " +
                         "COUNT(us.`user_id`) AS `count_games` FROM users_games AS us " +
@@ -51,11 +52,22 @@ public class Lab_Database_Access_With_JDBC {
         stmt.setString(1, name);
 
         ResultSet rs = stmt.executeQuery();
-        while(rs.next()){
+
+        rs.next();
+        if(rs.getString("user_name") != null) {
             System.out.println("User: " + rs.getString("user_name") + "\n"
                     + rs.getString("first_name") + " " + rs.getString("last_name")
                     + " has played " + rs.getInt("count_games"));
+        }else{
+            System.out.println("No such user exists");
         }
+    }
+
+    private static Properties getProperties() {
+        Properties props = new Properties();
+        props.setProperty("user", "root");
+        props.setProperty("password", "1234");
+        return props;
     }
 }
 
