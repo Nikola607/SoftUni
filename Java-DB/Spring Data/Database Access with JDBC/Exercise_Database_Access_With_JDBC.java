73a25f52162ef0d207384c141e95160bbeb24345
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-06-27 17:04:07 +0300

    Update Exercise_Database_Access_With_JDBC.java

--- Java-DB/Spring Data/Database Access with JDBC/Exercise_Database_Access_With_JDBC.java
+++ Java-DB/Spring Data/Database Access with JDBC/Exercise_Database_Access_With_JDBC.java
@@ -73,7 +73,91 @@ public class Exercise_Database_Access_With_JDBC {
     }
 
     private static void ex_3(Scanner scan) throws SQLException {
-        PreparedStatement stmt = connection.prepareStatement("");
+        String[] minionData = scan.nextLine().split("\\s+");
+        String[] villain = scan.nextLine().split("\\s+");
+
+        String minionName = minionData[1];
+        String minionAge = minionData[2];
+        String minionTown = minionData[3];
+
+        String villainName = villain[1];
+
+        PreparedStatement villainStmt = connection.prepareStatement("SELECT `name` FROM villains\n" +
+                "WHERE name = ?;");
+        villainStmt.setString(1, villainName);
+
+        PreparedStatement townStmt = connection.prepareStatement("SELECT `name` FROM towns\n" +
+                "WHERE name = ?;");
+        townStmt.setString(1, minionTown);
+
+        PreparedStatement minionStmt = connection.prepareStatement("SELECT `name` FROM villains\n" +
+                "WHERE name = ?;");
+        minionStmt.setString(1, minionName);
+
+        ResultSet rs = villainStmt.executeQuery();
+        if(!rs.next()){
+            System.out.printf("Villain %s was added to the database.\n", villainName);
+
+            PreparedStatement insertVillain = connection.prepareStatement("INSERT INTO villains(name, evilness_factor) VALUES (?, 'evil');");
+            insertVillain.setString(1, villainName);
+
+            insertVillain.execute();
+        }
+
+        rs = townStmt.executeQuery();
+        if(!rs.next()){
+            System.out.printf("Town %s was added to the database.", minionTown);
+
+            PreparedStatement insertTown = connection.prepareStatement("INSERT INTO towns(name) VALUES (?);");
+            insertTown.setString(1, minionTown);
+            insertTown.execute();
+        }
+        rs = minionStmt.executeQuery();
+        if(!rs.next()){
+            PreparedStatement stmt2 =
+                    connection.prepareStatement("INSERT INTO minions (name, age, town_id)  VALUES (?, ?, ?)");
+
+            PreparedStatement stsad =
+                    connection.prepareStatement("SELECT id FROM towns WHERE name = ?");
+
+            stsad.setString(1, minionTown);
+            ResultSet id = stsad.executeQuery();
+
+            id.next();
+            int townId = id.getInt(1);
+
+            stmt2.setString(1, minionName);
+            stmt2.setString(2, minionAge);
+            stmt2.setInt(3, townId);
+
+            stmt2.execute();
+            System.out.println("Minion " + minionName + " was added to db.");
+        }
+
+        PreparedStatement addMinionToVillain = connection.prepareStatement("INSERT INTO minions_villains(minion_id, villain_id) VALUES (?, ?);");
+
+        PreparedStatement getMinionId = connection.prepareStatement("SELECT `id` FROM `minions`" +
+                "WHERE `name` = ?");
+        getMinionId.setString(1, minionName);
+
+        rs = getMinionId.executeQuery();
+        rs.next();
+
+        int minionId = rs.getInt(1);
+
+        PreparedStatement getVillainId = connection.prepareStatement("SELECT `id` FROM `villains`" +
+                "WHERE `name` = ?");
+        getVillainId.setString(1, villainName);
+
+        rs = getVillainId.executeQuery();
+        rs.next();
+
+        int villainId = rs.getInt(1);
+
+        addMinionToVillain.setInt(1, minionId);
+        addMinionToVillain.setInt(2, villainId);
+        addMinionToVillain.execute();
+        System.out.printf("Successfully added %s to be minion of %s.", minionName, villainName);
     }
 
     private static Properties getProperties() {
