e8c77d0d6ffb95671c044fd24133e498d299d9a5
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-07-13 23:53:15 +0300

    Delete Java-DB/Spring Data/Spring Data Advanced Quering directory

--- Java-DB/Spring Data/Spring Data Advanced Quering/src/main/java/spring/lab/springintro/repositories/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package spring.lab.springintro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import spring.lab.springintro.models.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
