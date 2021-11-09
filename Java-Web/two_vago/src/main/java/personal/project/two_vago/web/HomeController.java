72f80fd6ea0c98d36b9a5d46218b7b9166b0f97f
Nikola Vuchev <nikola_vuchev@abv.bg>
2021-11-09 12:56:39 +0200

    index page setup

---
+++ Java-Web/two_vago/src/main/java/personal/project/two_vago/web/HomeController.java
@@ -0,0 +1,13 @@
+package personal.project.two_vago.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
